76676442b7be674226e4e69c4ee805643ad47dd5
Joseph Ortiz <joe@speakinginbits.com>
2020-01-29 16:33:48 -0800

    Add project files.

---
+++ LacamasFair/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LacamasFair.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
