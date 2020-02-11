9e72f51849547d14d4a13cabb3941e6bd669384d
KarloParagas <karloparagas@gmail.com>
2020-02-10 19:58:40 -0800

    Added the action method for the Community Center page

--- LacamasFair/Controllers/HomeController.cs
+++ LacamasFair/Controllers/HomeController.cs
@@ -43,6 +43,11 @@ namespace LacamasFair.Controllers
             return View();
         }
 
+        public IActionResult CommunityCenter() 
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
