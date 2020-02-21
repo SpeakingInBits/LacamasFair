4f66e833c579a89f4c44b0d3178c7bcc6d5cb34b
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-02-21 12:53:16 -0800

    Created main department model with notations based on Visio

---
+++ LacamasFair/Models/DepartmentModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LacamasFair.Models
+{
+    public class DepartmentModel
+    {
+
+        /// <summary>
+        /// This is the primary key that connects to ParentDept in the SubDept class.
+        /// </summary>
+        [Key]
+        [Required]
+        public int DepartmentId { get; set; }
+
+        /// <summary>
+        /// This is a required name of the Department, that will have a list of SubDepartment Names
+        /// </summary>
+        [Required]
+        [StringLength(75)]
+        public string DepartmenName { get; set; }
+
+
+    }
+}
