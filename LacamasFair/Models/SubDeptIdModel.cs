c1a2ccc1379ea0136f97654e3b0985fff2bc3527
KarloParagas <karloparagas@gmail.com>
2020-03-03 19:30:22 -0800

    Fixed the property name for SubDeptIdModel's ParentDept property to DepartmentId

--- LacamasFair/Models/SubDeptIdModel.cs
+++ LacamasFair/Models/SubDeptIdModel.cs
@@ -18,10 +18,10 @@ namespace LacamasFair.Models
         public int SubDeptId { get; set; }
 
         /// <summary>
-        /// Foreign Key, that links to DepartmentName primary table (The parent department)
+        /// Foreign Key, that links to DepartmentModel's DepartmentId property (The parent department)
         /// </summary>
         [Required(ErrorMessage = "A Department name is required")]
-        public string DepartmentName { get; set; }
+        public string DepartmentId { get; set; }
 
 
         /// <summary>
