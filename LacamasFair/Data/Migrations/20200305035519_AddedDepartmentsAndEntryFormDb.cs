d4bdfab315b864cf38d96061ceb1d3f4f6e96f52
KarloParagas <karloparagas@gmail.com>
2020-03-18 11:59:04 -0700

    Reverted commit delete old migrations

---
+++ LacamasFair/Data/Migrations/20200305035519_AddedDepartmentsAndEntryFormDb.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LacamasFair.Data.Migrations
+{
+    public partial class AddedDepartmentsAndEntryFormDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(maxLength: 75, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubDepartments",
+                columns: table => new
+                {
+                    SubDeptId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentId = table.Column<int>(nullable: false),
+                    SubDeptName = table.Column<string>(maxLength: 60, nullable: true),
+                    FairEntryRules = table.Column<string>(maxLength: 1000, nullable: true),
+                    DeptClasses = table.Column<int>(maxLength: 750, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubDepartments", x => x.SubDeptId);
+                    table.ForeignKey(
+                        name: "FK_SubDepartments_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "DepartmentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EntryForms",
+                columns: table => new
+                {
+                    SubDeptId = table.Column<int>(nullable: false),
+                    EntryFormId = table.Column<int>(nullable: false),
+                    FormFile = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntryForms", x => x.SubDeptId);
+                    table.ForeignKey(
+                        name: "FK_EntryForms_SubDepartments_SubDeptId",
+                        column: x => x.SubDeptId,
+                        principalTable: "SubDepartments",
+                        principalColumn: "SubDeptId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubDepartments_DepartmentId",
+                table: "SubDepartments",
+                column: "DepartmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntryForms");
+
+            migrationBuilder.DropTable(
+                name: "SubDepartments");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
