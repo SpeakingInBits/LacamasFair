a17dabe64ab39e229f52b7b27697bc1f14ba2dcc
KarloParagas <karloparagas@gmail.com>
2020-03-18 10:55:51 -0700

    Updated SubDeptIdModel. DeptClasses was the wrong data type.

--- LacamasFair/Models/SubDeptIdModel.cs
+++ LacamasFair/Models/SubDeptIdModel.cs
@@ -20,26 +20,29 @@ namespace LacamasFair.Models
         /// <summary>
         /// Foreign Key, that links to DepartmentModel's DepartmentId property (The parent department)
         /// </summary>
-        [Required(ErrorMessage = "A Department name is required")]
+        [Required]
         public int DepartmentId { get; set; }
-
 
         /// <summary>
         /// Department description (This is the animal department, etc etc)
         /// </summary>
+        [Required(ErrorMessage = "A Sub Department Name is required")]
         [StringLength(60)]
+        [Display(Name = "Sub Department Name")]
         public string SubDeptName { get; set; }
 
         /// <summary>
         /// Text of Fair Entry rules that should be able to be updated by admin
         /// </summary>
         [StringLength(1000)]
+        [Display(Name = "Fair Entry Rules")]
         public string FairEntryRules { get; set; }
 
         /// <summary>
         /// Classes should store information about current available department classes
         /// </summary>
         [StringLength(750)]
-        public int DeptClasses { get; set; }
+        [Display(Name = "Department Classes (Ex. SubDepartment: Baking, Classes: Pies, Cookies, etc)")]
+        public string DeptClasses { get; set; }
     }
 }
