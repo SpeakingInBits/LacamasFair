c27d6b098a0c962496069752b702fcbb491accbb
Joseph Ortiz <joe@speakinginbits.com>
2020-03-18 12:22:37 -0700

    Remake migrations

--- LacamasFair/Migrations/ApplicationDbContextModelSnapshot.cs
+++ LacamasFair/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4,16 +4,14 @@ using LacamasFair.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace LacamasFair.Data.Migrations
+namespace LacamasFair.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20200318191945_Initial")]
-    partial class Initial
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
