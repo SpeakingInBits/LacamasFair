ff81df1bdf1e5b3c20da34ab09534b7b0e779a41
KarloParagas <karloparagas@gmail.com>
2020-03-31 15:50:01 -0700

    Added FacilityRental class

---
+++ LacamasFair/Models/FacilityRental.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LacamasFair.Models
+{
+    public class FacilityRental
+    {
+        /// <summary>
+        /// Unique id for a single facility
+        /// </summary>
+        [Key]
+        [Required]
+        public int FacilityId { get; set; }
+
+        [Required]
+        public string FacilityName { get; set; }
+
+        [Required]
+        public string Price { get; set; }
+    }
+}
