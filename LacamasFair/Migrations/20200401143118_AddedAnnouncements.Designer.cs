2babbf6dc608f894637a88fd1ca5f1ac507d56ea
KarloParagas <karloparagas@gmail.com>
2020-04-01 09:53:22 -0700

    Added migration for announcement

--- LacamasFair/Migrations/20200401143118_AddedAnnouncements.Designer.cs
+++ LacamasFair/Migrations/20200401143118_AddedAnnouncements.Designer.cs
@@ -4,14 +4,16 @@ using LacamasFair.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LacamasFair.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200401143118_AddedAnnouncements")]
+    partial class AddedAnnouncements
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
