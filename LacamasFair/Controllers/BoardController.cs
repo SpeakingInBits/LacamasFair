badae4d89933289395e2d9a1b5a79033e7324dc7
Tanner Disney <disneytanner@gmail.com>
2020-05-02 12:56:14 -0700

    Created new controllers and setup context for both

---
+++ LacamasFair/Controllers/BoardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LacamasFair.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LacamasFair.Controllers
+{
+    public class BoardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BoardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ LacamasFair/Controllers/FacilityController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LacamasFair.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LacamasFair.Controllers
+{
+    public class FacilityController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public FacilityController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
