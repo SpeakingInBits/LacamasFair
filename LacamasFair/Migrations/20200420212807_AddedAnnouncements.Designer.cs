0b730ae1a5c527aa57adc58154ea201fae048dc0
Karlo Paragas <49321258+KarloParagas@users.noreply.github.com>
2020-05-28 15:45:35 -0700

    Issue#98 announcements (#101)
    * Added Announcement class
    
    * Implemented an announcement card and removed the old placeholder calendar
    
    * Added migration for announcement
    
    * Added a DbSet for announcements
    
    * Added DbHelper method for announcements
    
    * Added add/edit/delete announcement methods for admin
    
    * Added add/edit/delete views for announcements
    
    * Implemented carousel system for announcements
    
    * Updated migrations
    
    * Modified the date property's data type
    
    Co-authored-by: Joseph Ortiz <joeprogrammer88@users.noreply.github.com>

--- LacamasFair/Migrations/20200420212807_AddedAnnouncements.Designer.cs
+++ LacamasFair/Migrations/20200420212807_AddedAnnouncements.Designer.cs
@@ -4,14 +4,16 @@ using LacamasFair.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LacamasFair.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200420212807_AddedAnnouncements")]
+    partial class AddedAnnouncements
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -107,26 +109,6 @@ namespace LacamasFair.Migrations
                     b.HasKey("SubDeptId");
 
                     b.ToTable("EntryForms");
-                });
-
-            modelBuilder.Entity("LacamasFair.Models.FacilityRental", b =>
-                {
-                    b.Property<int>("FacilityId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-                    b.Property<string>("FacilityName")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(50)")
-                        .HasMaxLength(50);
-
-                    b.Property<double>("Price")
-                        .HasColumnType("float");
-
-                    b.HasKey("FacilityId");
-
-                    b.ToTable("FacilityRentals");
                 });
 
             modelBuilder.Entity("LacamasFair.Models.SubDeptClassModel", b =>
---
+++ LacamasFair/Migrations/20200420212807_AddedAnnouncements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LacamasFair.Migrations
+{
+    public partial class AddedAnnouncements : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Announcements",
+                columns: table => new
+                {
+                    AnnouncementId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 100, nullable: false),
+                    Description = table.Column<string>(maxLength: 1000, nullable: false),
+                    Date = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Announcements", x => x.AnnouncementId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Announcements");
+        }
+    }
+}
